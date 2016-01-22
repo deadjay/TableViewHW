d7c84d3d16f8ad4a2d73f9efce27f4c2783b5a87
deadjay <iaffector@gmail.com>
2016-01-22 18:44:55 +0300

    Initial Commit

---
+++ TableViewSearchHW/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewSearchHW
+//
+//  Created by Артем Алексеев on 1/22/16.
+//  Copyright (c) 2016 Artyom Alexeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
